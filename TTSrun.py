f406b71cb0e2013aa9b2d87f600ad0365033dcd1
Yaz12321 <40473045+Yaz12321@users.noreply.github.com>
2019-05-22 18:16:41 +0300

    Add files via upload

--- TTSrun.py
+++ TTSrun.py
@@ -2,6 +2,12 @@ import win32com.client as wincl
 import os
 path = os.path.dirname(os.path.abspath(__file__))
 speak = wincl.Dispatch("SAPI.SpVoice")
+
+
+speak.Rate = 3 ## Rate of speech: -10 (slowest) to 10 (fastest)
+speak.Volume = 100 ## Speech volume: 0 - 100
+
+
 print "hello"
 while True:
     f = open("TTS.txt","r+")
